25e08b873b85cdf63e820678e491fa498673bba7
Reza <76862881+0xflutter@users.noreply.github.com>
2024-02-21 02:05:52 +0330

    Update index.tsx

--- templates/HomePage/Website/index.tsx
+++ templates/HomePage/Website/index.tsx
@@ -22,8 +22,13 @@ const Website = ({ scrollToRef }: WebsiteProps) => {
                     <div className={cn("h5M", styles.info)}>
                     join waitlist now!
                     </div>
+                    function SandboxExample() return (
+    <div>
                     <iframe src="https://paragraph.xyz/@tayaswap/embed?minimal=true" width="480" height="45" style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"/>
-                </div>
+             </div>
+  );
+                    
+                    </div>
                 {/* <div className={styles.previewMobile}>
                     <Image
                         src="/images/website-pic-mobile.png"
